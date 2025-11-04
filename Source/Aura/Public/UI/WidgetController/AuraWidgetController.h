7bef33cb89acaef925aec9380e147a3c73ef8662
Caiowulf <caiowulf.ppm@gmail.com>
2025-11-04 12:50:42 +0100

    Create WBP_GlobeProgressBar
    Create Aura Widget WBP_GlobeProgressBar as a parent for inheritant Globe Progress Bars.
    Add some default values to variables creating a Health Progress Bar.

--- Source/Aura/Public/UI/WidgetController/AuraWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/AuraWidgetController.h
@@ -18,15 +18,15 @@ class AURA_API UAuraWidgetController : public UObject
 	GENERATED_BODY()
 protected:
 
-	UPROPERTY( BlueprintReadOnly, Category="WidgetController")
+	UPROPERTY(BlueprintReadOnly, Category="WidgetController")
 	TObjectPtr<APlayerController> PlayerController;
 
-	UPROPERTY( BlueprintReadOnly, Category="WidgetController")
+	UPROPERTY(BlueprintReadOnly, Category="WidgetController")
 	TObjectPtr<APlayerState> PlayerState;
 
-	UPROPERTY( BlueprintReadOnly, Category="WidgetController")
+	UPROPERTY(BlueprintReadOnly, Category="WidgetController")
 	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
 
-	UPROPERTY( BlueprintReadOnly, Category="WidgetController")
+	UPROPERTY(BlueprintReadOnly, Category="WidgetController")
 	TObjectPtr<UAttributeSet> AttributeSet;
 };
